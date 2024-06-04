429e52a616e588ea27717a1c86044db7fd19dbd6
Gabriel Bruno <114627827+GabrielBruno7@users.noreply.github.com>
2024-06-04 00:08:30 -0300

    variaveis

---
+++ 2 - Variáveis/variaveis.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Gabriel"
+
+	name := "Gabriel"
+
+	var (
+		name string = "Gabriel"
+		nickname string = "Biel"
+	)
+
+	name, nickname := "Gabriel", "Biel"
+}
